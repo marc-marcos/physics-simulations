fe849a4ed1465655b25eb1cc8de3e4ea13e8346a
Marc Marcos <marcmarcosmadruga@gmail.com>
2022-03-03 21:33:06 +0100

    velocity vector

--- MHS/mhs.py
+++ MHS/mhs.py
@@ -8,3 +8,11 @@ def movimentHarmonic(amplitud, freq, temps, faseInicial=0):
     a = amplitud * omega*omega * math.sin(omega * temps + faseInicial) * -1
 
     return [x, v, a]
+
+def maxMovimentHarmonic(amplitud, freq, temps, faseInicial=0):
+    omega = 2 * math.pi * freq
+
+    v = amplitud * omega
+    a = omega * omega * amplitud
+
+    return [v, a]
---
+++ MHS/mhsForces.py
@@ -0,0 +1,57 @@
+import mhs
+import math
+import pygame, pygame_gui
+import time
+import numpy
+
+pygame.init()
+
+pygame.display.set_caption('Quick Start')
+window_surface = pygame.display.set_mode((800, 600))
+
+background = pygame.Surface((800, 600))
+background.fill(pygame.Color('#000000'))
+
+manager = pygame_gui.UIManager((800, 600))
+
+clock = pygame.time.Clock()
+is_running = True
+
+startTime = time.time()
+
+while is_running:
+    time_delta = clock.tick(60)/1000.0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+
+        manager.process_events(event)
+
+    manager.update(time_delta)
+
+    window_surface.blit(background, (0, 0))
+    manager.draw_ui(window_surface)
+
+    timeNow = time.time()
+    timeElapsed = time.time() - startTime    
+
+    x = mhs.movimentHarmonic(300, 0.5, timeElapsed)
+    pygame.draw.circle(window_surface, (227, 11, 92), (400 + x[0], 300), 40, 0)
+
+    maxes = mhs.maxMovimentHarmonic(300, 0.5, timeElapsed)
+
+    relVel = x[1]/maxes[0]
+
+    if relVel >= 0:
+        pygame.draw.rect(window_surface, (0, 255, 0), (400 + x[0], 375, relVel * 200, 25))
+
+    else:
+        pygame.draw.rect(window_surface, (0, 255, 0), (400 + x[0] + relVel * 200, 375, relVel * -200, 25))
+        print(400 + x[0])
+        print(400 + x[0] + relVel * 200)
+        print()
+
+
+    pygame.display.update()
+
+ 
